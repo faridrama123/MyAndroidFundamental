ce026356c7455bc23144aec4db0a87327ded2789
faridrama123 <systemcalls915@gmail.com>
2021-03-22 20:59:48 +0700

    add view model

--- MyViewModel/app/src/main/java/com/faridrama123/myviewmodel/MainActivity.kt
+++ MyViewModel/app/src/main/java/com/faridrama123/myviewmodel/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.faridrama123.myviewmodel.databinding.ActivityMainBinding
 import com.loopj.android.http.AsyncHttpClient
@@ -15,6 +16,8 @@ import java.text.DecimalFormat
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter: WeatherAdapter
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +29,22 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
+        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
+
         binding.btnCity.setOnClickListener {
             val city = binding.editCity.text.toString()
             if (city.isEmpty()) return@setOnClickListener
             showLoading(true)
-            setWeather(city)
+            mainViewModel.setWeather(city)
         }
+
+        mainViewModel.getWeathers().observe(this, { weatherItems ->
+            if (weatherItems != null) {
+                adapter.setData(weatherItems)
+                showLoading(false)
+            }
+        })
 
     }
 
@@ -44,40 +57,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setWeather(cities: String) {
-        val listItems = ArrayList<WeatherItems>()
-        val apiKey = "c99911df55fd0b67d65b8eb6f7922766"
-        val url = "https://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric&appid=${apiKey}"
-        val client = AsyncHttpClient()
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
-                try {
-                    //parsing json
-                    val result = String(responseBody)
-                    val responseObject = JSONObject(result)
-                    val list = responseObject.getJSONArray("list")
-                    for (i in 0 until list.length()) {
-                        val weather = list.getJSONObject(i)
-                        val weatherItems = WeatherItems()
-                        weatherItems.id = weather.getInt("id")
-                        weatherItems.name = weather.getString("name")
-                        weatherItems.currentWeather = weather.getJSONArray("weather").getJSONObject(0).getString("main")
-                        weatherItems.description = weather.getJSONArray("weather").getJSONObject(0).getString("description")
-                        val tempInKelvin = weather.getJSONObject("main").getDouble("temp")
-                        val tempInCelsius = tempInKelvin - 273
-                        weatherItems.temperature = DecimalFormat("##.##").format(tempInCelsius)
-                        listItems.add(weatherItems)
-                    }
-                    //set data ke adapter
-                    adapter.setData(listItems)
-                    showLoading(false)
-                } catch (e: Exception) {
-                    Log.d("Exception", e.message.toString())
-                }
-            }
-            override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
-                Log.d("onFailure", error.message.toString())
-            }
-        })
-    }
+
 }
